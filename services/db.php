0c80826dc8fd5b50fd47ec82ff0c415512caaf49
Sarawut Pe <85542134+sarawutpe@users.noreply.github.com>
2022-07-21 21:25:49 +0700

    update db.php

--- services/db.php
+++ services/db.php
@@ -3,10 +3,10 @@ class DB {
     protected $db;
 
     function __construct(){
-        $db_host = "localhost";
-        $db_username = "root";
-        $db_password = "";
-        $db_dbname = "php_stock_db";
+        $db_host = "us-cdbr-east-06.cleardb.net";
+        $db_username = "b64e841bbd6ce3";
+        $db_password = "c6ffd493";
+        $db_dbname = "heroku_0995468ce18ab19";
 
         try {
             $conn = new PDO("mysql:host=$db_host;dbname=$db_dbname", $db_username, $db_password);
