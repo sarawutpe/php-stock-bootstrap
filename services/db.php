8a9943f76a105e1edc004acc65f932d178b3fe79
Sarawut Pe <85542134+sarawutpe@users.noreply.github.com>
2022-07-21 21:45:04 +0700

    up

--- services/db.php
+++ services/db.php
@@ -6,7 +6,7 @@ class DB {
         $db_host = "us-cdbr-east-06.cleardb.net";
         $db_username = "b64e841bbd6ce3";
         $db_password = "c6ffd493";
-        $db_dbname = "php_stock_db";
+        $db_dbname = "heroku_0995468ce18ab19";
 
         try {
             $conn = new PDO("mysql:host=$db_host;dbname=$db_dbname", $db_username, $db_password);
